04fa19dcb377debc8e641515d1ddadfb0c38c494
Harun Yilmaz <harun.yilmaz@sabanciuniv.edu>
2025-02-16 18:59:45 +0300

    fix: handle file opening error\nfeat: print matrix after reading from the file

--- main.cpp
+++ main.cpp
@@ -11,19 +11,19 @@
 #include <cassert>	// for assert()
 using namespace std;
 
-void getFileName(const int first, string& filename) {
+void GetFileName(const int first, string& filename) {
 	// Ask for the first time
 	if (first) {
 		cout << "Please enter the file name:" << endl;
 	}
 	// Ask again
 	else {
-		cout << "The file couldn't be opened.\nPlease enter a valid file name:" << endl;
+		cout << "The file couldn't be opened." << endl << "Please enter a valid file name:" << endl;
 	}
 	cin >> filename;
 }
 
-void readMatrix(ifstream& input, vector<vector<char>>& playground) {
+void ReadMatrix(ifstream& input, vector<vector<char>>& matrix) {
 	string line;
 	int expected_length = 0;
 	bool is_first_line = true;
@@ -38,7 +38,7 @@ void readMatrix(ifstream& input, vector<vector<char>>& playground) {
 			if (line.size() != expected_length) {
 				cout << "The matrix either has invalid dimensions or contains invalid characters." << endl
 					 << "Exiting the game. Bye bye." << endl;
-				exit(EXIT_FAILURE);i
+				exit(EXIT_FAILURE);
 			}
 		}
 
@@ -59,11 +59,22 @@ void readMatrix(ifstream& input, vector<vector<char>>& playground) {
 		}
 
 		// Add the valid line to the matrix
-		playground.push_back(line_vector);
+		matrix.push_back(line_vector);
 
 		#ifndef NDEBUG
 				cout << "A valid line is added to the matrix..." << endl;
 		#endif
+	}
+}
+
+void PrintMatrix(vector<vector<char>> matrix) {
+	// Row
+	for (int i = 0; i < matrix.size(); i++) {
+		// Column
+		for (int j = 0; j < matrix[0].size(); j++) {
+			cout << matrix[i][j];
+		}
+		cout << endl;
 	}
 }
 
@@ -75,17 +86,20 @@ int main() {
 	// Initialize variables
 	string					f_name;
 	vector<vector<char>>	playground;
+	ifstream				input;
 
 	// Get input file name
 	#ifndef NDEBUG
 		cout << "Getting input file name from cli..." << endl;
 	#endif
-	getFileName(1, f_name);
+	GetFileName(1, f_name);
+	input.open(f_name);
 
 	// Try to open the file until success
-	ifstream	input(f_name);
 	while ( input.fail() ) {
-		getFileName(0, f_name);
+		GetFileName(0, f_name);
+		input.clear();
+		input.open(f_name);
 	}
 
     // Read from the file
@@ -94,11 +108,13 @@ int main() {
 		cout << f_name << " is open. Ready to read from the file..." << endl;
 	#endif
 
-	readMatrix(input, playground);
+	ReadMatrix(input, playground);
 
 	#ifndef NDEBUG
 		assert( !playground.empty() );		// The matrix should not be empty
-		cout << "The file is read to the matrix..." << endl;
+		cout << "The file is read to the matrix. Ready to print the matrix..." << endl;
 	#endif
+	cout << "The content of the matrix is:" << endl;
+	PrintMatrix(playground);
 
 }
